ec0c87081ea4c6ecf4a00c26a561d9915e4657c4
Sander van de Graaf <mail@svdgraaf.nl>
2016-09-26 16:00:13 +0200

    initial import

---
+++ src/index.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const _ = require('lodash');
+const Class = require('class.extend');
+const chalk = require('chalk');
+
+module.exports = Class.extend({
+
+  init: function(serverless, opts) {
+    this._serverless = serverless;
+    this._opts = opts;
+    this._SDK = null;
+    const that = this;
+
+    // find the serverless sdk
+    _.forEach(this._serverless.pluginManager.plugins, function(plugin){
+      if (plugin.constructor.name == 'AwsInfo') {
+        // got the info plugin
+        that._SDK = plugin.sdk
+      }
+    })
+
+    this.hooks = {
+      'after:deploy:deploy': this.outputStackOutput.bind(this),
+      'after:info:info': this.outputStackOutput.bind(this),
+    };
+  },
+
+  outputStackOutput: function() {
+    const stackName = `${this._serverless.service.service}-${this._opts.stage}`;
+    var foo = this._SDK.request('CloudFormation',
+      'describeStacks',
+      {
+        StackName: stackName
+      },
+      this._opts.stage,
+      this._opts.region
+    ).then((result) => {
+      if (result) {
+        const outputs = result.Stacks[0].Outputs;
+
+        var msg = `${chalk.yellow.underline('Stack output:')}\n`;
+        _.forEach(outputs, function(output){
+          msg = msg.concat(`${chalk.yellow(output.OutputKey)}: ${output.OutputValue}\n`);
+        });
+
+        this._serverless.cli.consoleLog(msg);
+      }
+    });
+  },
+});
